3dce78c6c82b8f27ef34423a5e379dba047ebc04
Leonardo Arellano <leo.aremtz98@gmail.com>
2018-06-02 13:27:18 -0500

    Commit de Leo

--- Complejos.cpp
+++ Complejos.cpp
@@ -35,52 +35,6 @@ class SumaResta{
 };
 
 main(){
-	SumaResta T[tam];
-	char masop;
-	int i=2;
-	T[1].Asignar();
-	cout<<"Sumar/Restar [+/-]: "; cin>>masop;
-	for (int k=0; k<tam; k++){
-		if (masop == '+'){
-			T[i].Asignar();
-			if(i==2){
-				T[0].Suma(T[1], T[i]);
-			}
-			else{
-				T[0].Suma(T[0], T[i]);
-			}
-			for(int j=1; j<=i; j++){
-				cout<<"Numero "<<j<<": ";
-				T[j].Imprimir();
-			}
-			i++;
-			cout<<"Sumar/Restar/Total [+/-/=]: "; cin>>masop;
-			}
-		else if(masop == '-'){
-			T[i].Asignar();
-			if(i==2){
-				T[0].Resta(T[1], T[i]);
-			}
-			else{
-				T[0].Resta(T[0], T[i]);
-			}
-			for(int j=1; j<=i; j++){
-				cout<<"Numero "<<j<<": ";
-				T[j].Imprimir();
-			}
-			i++;
-			cout<<"Sumar/Restar/Total [+/-/=]: "; cin>>masop;
-		}
-		else
-			k=51;
-	}
-	if(i==2){
-		cout<<"Total: "<<endl;
-		T[1].Imprimir();
-	}
-	else{
-		cout<<"Total: "<<endl;
-		T[0].Imprimir();
-	}
+	cout << "Hola " << endl;
 	getch();
 }
